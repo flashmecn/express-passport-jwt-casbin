8dafcb0cd6e3b13e16b427cba03cca79e1fd8d8b
flashmecn <flashme@live.cn>
2020-07-13 17:13:45 +0800

    add mail

--- myapp/routes/login.js
+++ myapp/routes/login.js
@@ -9,6 +9,7 @@ const rateLimit = require("express-rate-limit");
 var pass = require('../common/passport');
 var user = require('../common/user');
 var core = require('../common/core');
+var toemail = require('../common/email');
 
 
 /* 登录页面. */
@@ -58,7 +59,7 @@ router.post('/', apiLimiter, function (req, res, next) {
             // res.status(401).json({ msg: "密码错误！" });
             // res.redirect('/error');
         }
-    },function () {
+    }, function () {
         res.json({ msg: "没有此用户！" });
     })
 });
@@ -164,5 +165,83 @@ router.post('/useradd', limiter, function (req, res, next) {
     })
 })
 //===============================================用户注册END
+
+
+//================找回密码
+
+const mailLimiter = rateLimit({
+    windowMs: 60 * 1000, // 1分钟
+    max: 1, // 限制请求次数
+    message: "请1分钟后重试"
+});
+router.post('/repass', mailLimiter, function (req, res, next) {
+    var email = req.body.email.trim();
+    var regEmail = core.confirmEmail(email);
+    if (regEmail) {
+        res.json({ msg: regEmail });
+        return;
+    }
+
+    //检查账户
+    var verify, userid;
+    user.userget({ email: [email] }).then(function (row) {
+        if (!row[0] || !row[0].email || row[0].email != email) {
+            res.json({ msg: "此邮箱未注册！" });
+            return;
+        }
+        userid = row[0].id;
+        verify = core.randomString(6);//生成随机码
+        //发送邮件
+        return toemail.sendmail({
+            subject: 'flashme.cn 密码找回验证', to: '"用户1" <' + email + '>',
+            html: '<div style="width: 300px;margin: 20px auto;line-height: 1.7;background: #eee;text-align: center;"><h3>验证码</h3><h2>' + verify + '</h2><h4>此邮件来自：<a href="http://www.flashme.cn">flashme.cn</a></h4></div>'
+        })
+
+    }).then(function () {
+        req.session.verify_code = verify;
+        req.session.verify_email = email;
+        req.session.verify_id = userid;
+        res.json({ state: true, msg: "发送成功" });
+    }, function (err) {
+        res.json({ state: false, msg: "发送失败！" + err });
+    });
+})
+
+const mailLimiter2 = rateLimit({
+    windowMs: 60 * 1000, // 1分钟
+    max: 3, // 限制请求次数
+    message: "多次验证错误！请1分钟后重试",
+    onLimitReached: function (req, res, options) {
+        res.json({ msg: options.message });
+    }
+});
+router.post('/repassword', mailLimiter2, function (req, res, next) {
+    var email = req.body.email.trim();
+    var verify = req.body.verify.trim();
+    var password = req.body.password.trim();
+    if (email != req.session.verify_email || verify != req.session.verify_code) {
+        res.json({ msg: "验证失败！" });
+        return;
+    }
+    if (password && password.length < 6) {
+        res.json({ msg: "密码需至少6位！" });
+        return;
+    }
+    // Hmac加密
+    var hash = crypto.createHmac('sha512', core.key)
+    hash.update(password)
+    var miwen = hash.digest('hex')
+
+    var updata = { id: req.session.verify_id, data: { password: miwen } }
+    user.useredit(updata).then(function (err) {
+        if (!err) {
+            res.json({ state: true, msg: "修改成功" });
+            // pass.resetUser();
+        } else {
+            res.json({ state: false, msg: "修改失败！" });
+        }
+    })
+})
+
 
 module.exports = router;
